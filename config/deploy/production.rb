e8fae8f05738ddea589b6f6f7d5fe95f5b90d3e2
Ivan Linko <linko.ivan@gmail.com>
2012-09-21 22:45:16 +0300

    update multistage settings

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,13 @@
-set :rails_env, "production"
-set :deploy_env, 'production'
+set :rails_env,   "production"
+set :deploy_env,  'production'
 
+set :application, "app"
+set :domain,      'app.com'
+set :repository,  "git@repo"
+
+set :deploy_to,   "/home/#{user}/apps/#{application}"
+set :branch,      "master"
+
+role :app, domain
+role :web, domain
+role :db,  domain, primary: true
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,13 @@
-set :rails_env, "staging"
-set :deploy_env, 'staging'
+set :rails_env,   "staging"
+set :deploy_env,  'staging'
+
+set :application, "app"
+set :domain,      'app.com'
+set :repository,  "git@repo"
+
+set :deploy_to,   "/home/#{user}/apps/staging.#{application}"
+set :branch,      "develop"
+
+role :app, domain
+role :web, domain
+role :db,  domain, primary: true
