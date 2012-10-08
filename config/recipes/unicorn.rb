a8dd411b97aefcc5b2cc6110b4702e965d4fb1ca
Ivan Linko <linko.ivan@gmail.com>
2012-10-08 21:23:45 +0300

    added unicorn strategy for start/stop tasks

--- config/recipes/unicorn.rb
+++ config/recipes/unicorn.rb
@@ -1,5 +1,5 @@
 set_default(:unicorn_user) { user }
-set_default(:unicorn_pid) { "#{current_path}/tmp/pids/unicorn.pid" }
+set_default(:unicorn_pid) { "#{shared_path}/pids/unicorn.pid" }
 set_default(:unicorn_config) { "#{shared_path}/config/unicorn.rb" }
 set_default(:unicorn_log) { "#{shared_path}/log/unicorn.log" }
 set_default(:unicorn_workers, 2)
@@ -11,16 +11,30 @@ namespace :unicorn do
     template "unicorn.rb.erb", unicorn_config
     template "unicorn_init.erb", "/tmp/unicorn_init"
     run "chmod +x /tmp/unicorn_init"
-    run "#{sudo} mv /tmp/unicorn_init /etc/init.d/unicorn_#{application}"
-    run "#{sudo} update-rc.d -f unicorn_#{application} defaults"
+    run "#{sudo} mv /tmp/unicorn_init /etc/init.d/unicorn_#{application}_#{deploy_env}"
+    run "#{sudo} update-rc.d -f unicorn_#{application}_#{deploy_env} defaults"
   end
   after "deploy:setup", "unicorn:setup"
 
   %w[start stop restart].each do |command|
     desc "#{command} unicorn"
     task command, roles: :app do
-      run "service unicorn_#{application} #{command}"
+      run "service unicorn_#{application}_#{deploy_env} #{command}"
     end
-    after "deploy:#{command}", "unicorn:#{command}"
+    #after "deploy:#{command}", "unicorn:#{command}"
+  end
+
+
+end
+
+namespace :deploy do
+  task :restart do
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_config} -E #{rails_env} -D; fi"
+  end
+  task :start do
+    run "cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_config} -E #{rails_env} -D"
+  end
+  task :stop do
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
   end
 end
