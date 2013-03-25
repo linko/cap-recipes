350d02d8648ec74b575f57f8543e2e60f8402899
Ivan Linko <linko.ivan@gmail.com>
2013-03-25 12:58:53 +0200

    remove unused fields from env configs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,10 +2,7 @@ set :rails_env,   "production"
 set :deploy_env,  'production'
 
 set :domain,      'app.com'
-set :repository,  "git@repo"
-
 set :deploy_to,   "/home/#{user}/apps/#{domain}"
-set :branch,      "master"
 
 role :app, domain
 role :web, domain
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,10 +2,7 @@ set :rails_env,   "staging"
 set :deploy_env,  'staging'
 
 set :domain,      'app.com'
-set :repository,  "git@repo"
-
 set :deploy_to,   "/home/#{user}/apps/staging.#{domain}"
-set :branch,      "develop"
 
 role :app, domain
 role :web, domain
