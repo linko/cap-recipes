99784098194cfb7c400c75711853f7edb319f3df
Ivan Linko <linko.ivan@gmail.com>
2013-06-24 01:34:51 +0300

    Update env config with default branch, fix ruby version

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :rails_env,   "production"
 set :deploy_env,  'production'
 
-set :domain,      'app.com'
+set :branch,      'master'
 set :deploy_to,   -> { "/home/#{user}/apps/#{domain}" }
 
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :rails_env,   "staging"
 set :deploy_env,  'staging'
 
-set :domain,      'app.com'
+set :branch,      'master'
 set :deploy_to,   -> { "/home/#{user}/apps/staging.#{domain}" }
