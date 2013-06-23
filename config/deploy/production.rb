a10fcf98c6c806e4a1a90d81ee6850f911a3fe16
Ivan Linko <linko.ivan@gmail.com>
2013-06-24 01:06:52 +0300

    DRY, added symlink task to deploy.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,8 +2,5 @@ set :rails_env,   "production"
 set :deploy_env,  'production'
 
 set :domain,      'app.com'
-set :deploy_to,   "/home/#{user}/apps/#{domain}"
+set :deploy_to,   -> { "/home/#{user}/apps/#{domain}" }
 
-role :app, domain
-role :web, domain
-role :db,  domain, primary: true
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,8 +2,4 @@ set :rails_env,   "staging"
 set :deploy_env,  'staging'
 
 set :domain,      'app.com'
-set :deploy_to,   "/home/#{user}/apps/staging.#{domain}"
-
-role :app, domain
-role :web, domain
-role :db,  domain, primary: true
+set :deploy_to,   -> { "/home/#{user}/apps/staging.#{domain}" }
