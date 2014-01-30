13689e819e3754f893a33f6bbf7a0610eec63925
Ivan Linko <linko.ivan@gmail.com>
2014-01-30 21:19:35 +0200

    Updates to configs

--- config/recipes/unicorn.rb
+++ config/recipes/unicorn.rb
@@ -1,5 +1,5 @@
 set_default(:unicorn_user)    { user }
-set_default(:unicorn_pid)     { "#{shared_path}/pids/unicorn.pid" }
+set_default(:unicorn_pid)     { "#{shared_path}/tmp/pids/unicorn.pid" }
 set_default(:unicorn_config)  { "#{shared_path}/config/unicorn.rb" }
 set_default(:unicorn_log)     { "#{shared_path}/log/unicorn.log" }
 set_default(:unicorn_workers, 2)
