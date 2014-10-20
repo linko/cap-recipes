4d033029edbc4500a1fc19da2b7f8c105494b6cc
Ivan Linko <linko.ivan@gmail.com>
2014-10-20 11:18:52 +0400

    Updates to basefile

--- config/recipes/base.rb
+++ config/recipes/base.rb
@@ -13,18 +13,19 @@ set_default :general_packages, <<-EOS
 EOS
 
 namespace :deploy do
-  desc "Install everything onto the server"
+  desc 'Install everything onto the server'
   task :install do
     run "#{sudo} apt-get -y update"
     run "#{sudo} apt-get -y install python-software-properties"
     run "#{sudo} apt-get -y install nodejs"
     run "#{sudo} apt-get -y install libcurl3-dev"
+    run "#{sudo} apt-get -y install imagemagick"
 
 
     run "#{sudo} apt-add-repository ppa:blueyed/ppa",:pty => true do |ch, stream, data|
       if data =~ /Press.\[ENTER\].to.continue/
         #prompt, and then send the response to the remote process
-        ch.send_data(Capistrano::CLI.password_prompt("Press [ENTER] to continue") + "\n")
+        ch.send_data(Capistrano::CLI.password_prompt('Press [ENTER] to continue') + "\n")
       else
         #use the default handler for all other text
         Capistrano::Configuration.default_io_proc.call(ch,stream,data)
