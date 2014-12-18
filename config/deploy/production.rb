4e89651154cb19e04e6f840fd13632215735b374
Ivan Linko <linko.ivan@gmail.com>
2014-12-18 10:28:59 +0300

    Use env setter by default

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :rails_env,   'production'
 set :branch,      'master'
 set :deploy_to,   -> { "/home/#{user}/apps/#{domain_name}" }
 
-#top.env.current_environment.set 'RAILS_ENV', 'production'
+top.env.current_environment.set 'RAILS_ENV', 'production'
